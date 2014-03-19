fed14d4fc03f6aa7625941687b7593c7aaa61d94
suisho <suisho000000@gmail.com>
2014-03-20 02:14:50 +0900

    comment out combinator parser

--- index.js
+++ index.js
@@ -7,35 +7,13 @@ var document = jsdom("<html>")
 module.exports = function(selector){
   var htmls = []
   var parsed = parser(selector)
-  parsed = combinatorProc(parsed)
+  //parsed = combinatorProc(parsed)
   parsed.forEach(function(selectorObj){
     htmls.push(buildDom(selectorObj).innerHTML)
   })
   return htmls.join("")
 }
 
-function combinatorProc(selectorObjects){
-  var output = []
-  selectorObjects.forEach(function(selectorBlock){
-    var processed = []
-    var after = []
-    selectorBlock.forEach(function(selector){
-      switch(selector.combinator){
-        case '+':
-          after.push(selector)
-          break
-        default:
-          processed.push(selector)
-      }
-    })
-    // push
-    output.push(processed)
-    if(after.length > 0){
-      output.push(after)
-    }
-  })
-  return output
-}
 
 // DOM Builder
 function setDomAttributes(dom, attrs){
