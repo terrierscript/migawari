9028c687775087da480c5066f6af50fbca74e0f1
suisho <suisho000000@gmail.com>
2014-03-25 00:47:48 +0900

    fix failure when .foo[class=baz]

--- test/index.js
+++ test/index.js
@@ -1,41 +1,40 @@
 var migawari = require('../index.js')
 var assert = require('assert')
 var cheerio = require('cheerio')
-var assertCssSelector = function(selector){
-  var html = migawari(selector)
-  var $ = cheerio.load("<html>"+html+"</html>")
-  assert($(selector).length > 0)
-}
-var t = function(selector, html, comment){
+var assertCssSelector = require("./assertLib")
+var t = function(selector, comment){
   comment = comment || ""
   it(selector +" "+ comment,  function(){
-    assert.equal(migawari(selector), html)
+    //assert.equal(migawari(selector), html)
     assertCssSelector(selector)
   })
 }
 
 describe('basics', function(){
-  t('a','<a></a>', 'only tag')
-  t('*','<div></div>')
-  t('.foo','<div class="foo"></div>', 'only class')
-  t('a.foo','<a class="foo"></a>','class and tag')
-  t('.foo.bar','<div class="bar foo"></div>', 'multi classes')
-  t('.foo, .bar','<div class="foo"></div><div class="bar"></div>', 'parallel classes')
-  t('#someid','<div id="someid"></div>', 'id')
+  t('a', 'only tag') //<a></a>
+  t('*') //<div></div>
+  t('.foo', 'only class') //<div class="foo"></div>
+  t('a.foo','class and tag')
+  t('.foo.bar', 'multi classes')
+  t('.foo, .bar','parallel classes')
+  t('#someid','id')
 })
 
 describe('combinators', function(){
-  t('a > b','<a><b></b></a>', 'child')
-  t('a b','<a><b></b></a>', 'ancestor')
-  t('a + b','<a></a><b></b>', 'sibilings')
-  t('a b + p','<a><b></b><p></p></a>', 'ancestor and sibilings')
+  t('a > b', 'child')
+  t('a b', 'ancestor')
+  t('a + b', 'sibilings')
+  t('a b + p', 'ancestor and sibilings')
+  t('.a + b .c ', 'sibilings ancestor')
 
 })
 
 describe('attributes', function(){
-  t('a.foo[href=hoge]','<a class="foo" href="hoge"></a>')
-  t('a.foo[href~=hoge]','<a class="foo" href="hoge"></a>')
-  t('a.foo[href$=hoge]','<a class="foo" href="hoge"></a>')
-  t('a.foo[href=hoge][title="fuga"]','<a class="foo" href="hoge" title="fuga"></a>')
-  t('a.foo[href]','<a class="foo" href="href"></a>')
+  t('a.foo[href=hoge]')
+  t('a.foo[href~=hoge]')
+  t('a.foo[href$=hoge]')
+  t('a.foo[href*=hoge]')
+  t('a.foo[href=hoge][title="fuga"]')
+  t('a.foo[href]')
+  t('a.foo[class="baz"]', 'attr has class')
 })
