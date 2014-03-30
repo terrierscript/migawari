11ae50bf4577450a8db63a3ffaf477020ad79397
suisho <suisho000000@gmail.com>
2014-03-31 01:24:09 +0900

    clean up node

---
+++ test/node.js
@@ -0,0 +1,27 @@
+var node = require('../lib/selector/node')
+var parser = require("../lib/parser")
+var render = require('cheerio/lib/render')
+var assert = require('assert')
+
+var createNode = function(selector){
+  var p = parser(selector)[0][0]
+  var n = node(p)
+  return render(n)
+}
+describe('node output', function(){
+  it('only tag', function(){
+    assert.equal( createNode("a"), "<a></a>")
+  })
+  it('only class', function(){
+    assert.equal( createNode(".foo"), '<div class="foo"></div>')
+  })
+  it('class attr', function(){
+    assert.equal( createNode('.foo[class="bar"]'), '<div class="bar foo"></div>')
+  })
+  it("id", function(){
+    assert.equal( createNode('#baz'), '<div id="baz"></div>')
+  })
+  it("attr", function(){
+    assert.equal( createNode('img[title="boo"]'), '<img title="boo">')
+  })
+})
