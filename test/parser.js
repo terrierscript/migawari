df660c6b1eab7be8f4d717d8f318f9d16b3b1b28
suisho <suisho000000@gmail.com>
2014-04-06 03:40:04 +0900

    fix test

--- test/parser.js
+++ test/parser.js
@@ -2,53 +2,55 @@ var parser = require("../lib/parser")
 var assert = require("assert")
 describe("parser", function(){
   it("", function(){
-    var result = parser("a b + .c > #d .e[f='g'],.h")
-    //console.log(require("util").inspect(result, {depth :null}))
-    var expect = [
-    [
-      { tag: { type: 'tag', name: 'a' },
-        combinator: undefined,
-        attributes: [] },
-      { tag: { type: 'tag', name: 'b' },
-        combinator: { type: 'descendant' },
-        attributes: [] },
-      { tag: undefined,
-        combinator: { type: 'adjacent' },
-        attributes:
-         [ { type: 'attribute',
-             name: 'class',
-             action: 'element',
-             value: 'c',
-             ignoreCase: false } ] },
-      { tag: undefined,
-        combinator: { type: 'child' },
-        attributes:
-         [ { type: 'attribute',
-             name: 'id',
-             action: 'equals',
-             value: 'd',
-             ignoreCase: false } ] },
-      { tag: undefined,
-        combinator: { type: 'descendant' },
-        attributes:
-         [ { type: 'attribute',
-             name: 'class',
-             action: 'element',
-             value: 'e',
-             ignoreCase: false },
-           { type: 'attribute',
-             name: 'f',
-             action: 'equals',
-             value: 'g',
-             ignoreCase: false } ] } ],
-    [ { tag: undefined,
-        combinator: undefined,
-        attributes:
-         [ { type: 'attribute',
-             name: 'class',
-             action: 'element',
-             value: 'h',
-             ignoreCase: false } ] } ] ]
+    var result = parser("a b + .c > #d ~ .e[f='g'],.h")
+    console.log(require("util").inspect(result, {depth :null}))
+    var expect = [ [ { tag: { type: 'tag', name: 'a' },
+      combinator: { type: 'child' },
+      attributes: [] },
+    { tag: undefined, combinator: { type: 'child' }, attributes: [] },
+    { tag: { type: 'tag', name: 'b' },
+      combinator: { type: 'adjacent' },
+      attributes: [] },
+    { tag: undefined,
+      combinator: { type: 'child' },
+      attributes:
+       [ { type: 'attribute',
+           name: 'class',
+           action: 'element',
+           value: 'c',
+           ignoreCase: false } ] },
+    { tag: undefined,
+      combinator: { type: 'adjacent' },
+      attributes:
+       [ { type: 'attribute',
+           name: 'id',
+           action: 'equals',
+           value: 'd',
+           ignoreCase: false } ] },
+    { tag: undefined,
+      combinator: { type: 'adjacent' },
+      attributes: [] },
+    { tag: undefined,
+      combinator: undefined,
+      attributes:
+       [ { type: 'attribute',
+           name: 'class',
+           action: 'element',
+           value: 'e',
+           ignoreCase: false },
+         { type: 'attribute',
+           name: 'f',
+           action: 'equals',
+           value: 'g',
+           ignoreCase: false } ] } ],
+  [ { tag: undefined,
+      combinator: undefined,
+      attributes:
+       [ { type: 'attribute',
+           name: 'class',
+           action: 'element',
+           value: 'h',
+           ignoreCase: false } ] } ] ]
     assert.deepEqual(result, expect)
   })
 })
