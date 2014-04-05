0cd61a4f99689fecff994a45a1950fdb2efc8a52
suisho <suisho000000@gmail.com>
2014-04-05 23:01:17 +0900

    traverse create dom

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //var defaults = require('defaults')
 var parser = require('./lib/parser')
 var tree = require('./lib/selector/tree')
+var node = require("./lib/selector/node")
 var defaults = require('defaults')
 var render = require('cheerio/lib/render')
-
+var traverse = require("traverse")
 var Migawari = function(selector){
   this.selector = selector
   this.parsed = parser(selector)
-  // function
-  this.domTree = tree(this.parsed)
+  var t = tree(this.parsed)
 
+  // node to dom
+  this.domTree = traverse(t).map(function(x){
+    if(!x || !x.children) return x;
+    var dom = node(x.selector)
+    dom.children = x.children
+    return dom
+  })
 }
 
 Migawari.prototype.toString = function(){
