83e6825e029aa40b480fd569942d8954df9e7633
suisho <suisho000000@gmail.com>
2014-04-10 01:48:18 +0900

    add dom test

---
+++ test/dom.js
@@ -0,0 +1,19 @@
+var migawari = require('../index.js')
+var assert = require('assert')
+var htmlparser = require("htmlparser2");
+
+describe('asset dom', function(){
+  var reparse = function(selector){
+    var m = migawari(selector)
+    var p = htmlparser.parseDOM(m.toString())
+    return p
+  }
+  var assertDOM = function(selector){
+    var m = migawari(selector)
+    var p = htmlparser.parseDOM(m.toString())
+    assert.deepEqual(m.dom, p)
+  }
+  it("basic", function(){
+    assertDOM("a")
+  })
+})
