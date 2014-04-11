63f0bf8e2f9a70f0b59945803579b58e6a03bfef
suisho <suisho000000@gmail.com>
2014-04-11 23:30:57 +0900

    fix leaf test

--- test/leaf.js
+++ test/leaf.js
@@ -1,27 +1,66 @@
 var leaf = require('../lib/selector/leaf')
 var parser = require("../lib/parser")
-var render = require('cheerio/lib/render')
 var assert = require('assert')
 
-var createNode = function(selector){
+var createLeaf = function(selector){
   var p = parser(selector)[0][0]
   var n = leaf(p)
-  return render(n)
+  return n
 }
 describe('leaf output', function(){
   it('only tag', function(){
-    assert.equal( createNode("a"), "<a></a>")
+    assert.deepEqual( createLeaf("a"), {
+      type:"tag",
+      name:"a",
+      attribs:{},
+      children:[],
+      parent:null,
+      prev:null,
+      next:null
+    })
   })
   it('only class', function(){
-    assert.equal( createNode(".foo"), '<div class="foo"></div>')
+    assert.deepEqual( createLeaf(".foo"), {
+      type: 'tag',
+      name: null,
+      attribs: { class: 'foo' },
+      children: [],
+      parent: null,
+      prev: null,
+      next: null
+    })
   })
   it('class attr', function(){
-    assert.equal( createNode('.foo[class="bar"]'), '<div class="foo bar"></div>')
+    assert.deepEqual( createLeaf('.foo[class="bar"]'),{
+      type: 'tag',
+      name: null,
+      attribs: { class: 'foo bar' },
+      children: [],
+      parent: null,
+      prev: null,
+      next: null
+    })
   })
   it("id", function(){
-    assert.equal( createNode('#baz'), '<div id="baz"></div>')
+    assert.deepEqual( createLeaf('#baz'), {
+      type: 'tag',
+      name: null,
+      attribs: { id: 'baz' },
+      children: [],
+      parent: null,
+      prev: null,
+      next: null
+    })
   })
   it("attr", function(){
-    assert.equal( createNode('img[title="boo"]'), '<img title="boo">')
+    assert.deepEqual( createLeaf('img[title="boo"]'),{
+      type: 'tag',
+      name: 'img',
+      attribs: { title: 'boo' },
+      children: [],
+      parent: null,
+      prev: null,
+      next: null
+    })
   })
 })
