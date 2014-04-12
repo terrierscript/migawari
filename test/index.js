d9a4ebe2e985446d51098446954f24c5740fe935
suisho <suisho000000@gmail.com>
2014-04-13 00:41:33 +0900

    remove test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var migawari = require("../index")
 var assert = require("assert")
 var htmlparser = require("htmlparser2")
+
 describe("properties", function(){
   it("rawDom props", function(){
     var m = migawari("a b")
@@ -9,14 +10,5 @@ describe("properties", function(){
   it("dom props", function(){
     var m = migawari("a b")
     assert.strictEqual(m.dom[0].children[0].name, "div")
-  })
-  it("htmlparser compatible", function(){
-    var objToString = function(obj){
-      return require("util").inspect(obj, {depth:null}).toString()
-    }
-    var m = migawari("a > b + p")
-    var htp = htmlparser.parseDOM(m.toString())
-    //console.log(require("util").inspect(m.dom, {depth:null}))
-    //assert.deepEqual(objToString(m.dom), objToString(htp))
   })
 })
